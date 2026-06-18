cfbd7d713340b98b1c65cfc7fdcefdf34f76d214
menatalh <mesaid@noon.com>
2026-06-18 10:08:01 +0300

    Changes 1

---
+++ test.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http')
+
+app.get('/', (req, res) => {
+    res.send('Hello From Express')
+})
+
+const server = app.listen(5000, () => {
+    console.log('Server started, running test...')
+
+    http.get('http://localhost:5000/', (res) => {
+        console.log(`Status: ${ res.statusCode }`);
+    })
+})
